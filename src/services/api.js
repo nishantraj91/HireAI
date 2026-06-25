6dc4fa9ec5c48fd9d41636ebada562d6ac35f795
nishantraj91 <niushantrajkhairi@gmail.com>
2026-06-25 15:34:34 +0530

    connect frontend to render backend api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://hireai-backend-hpgx.onrender.com/api",
 });
 
 // Har request se pehle JWT token attach karo
